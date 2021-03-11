396cdf678d0a3612963d1ac263c5bf549755fd75
Piotr <sixtyto@gmail.com>
2021-03-11 21:06:36 +0100

    move project info to separate component

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,7 @@
   <div class="container">
     <div class="row">
       <board-controls />
-
-      <div class="row" v-if="isReady">
-        <div class=" col-sm-10 offset-sm-1 text_wrapper">
-          <p>Name: {{ projectName }}</p>
-          <p>Id: {{ projectId }}</p>
-        </div>
-      </div>
-
+      <project-info />
       <hr />
 
       <the-board />
@@ -19,14 +12,15 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { mapState } from "vuex";
 import TheBoard from "./components/TheBoard.vue";
 import BoardControls from "./components/BoardControls.vue";
+import ProjectInfo from "./components/ProjectInfo.vue";
 
 export default defineComponent({
   components: {
     TheBoard,
-    BoardControls
+    BoardControls,
+    ProjectInfo
   },
   computed: {
     id: {
@@ -36,17 +30,7 @@ export default defineComponent({
       set(value: string) {
         this.$store.commit("setId", value);
       }
-    },
-    projectName(): string {
-      return this.projectInfo.name;
-    },
-    projectId(): string {
-      return this.projectInfo.id;
-    },
-    isReady(): boolean {
-      return this.projectInfo.items?.length !== 0;
-    },
-    ...mapState(["projectInfo"])
+    }
   }
 });
 </script>
---
+++ src/components/ProjectInfo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row" v-if="isReady">
+    <div class=" col-sm-10 offset-sm-1 text_wrapper">
+      <p>Name: {{ projectName }}</p>
+      <p>Id: {{ projectId }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { mapState } from "vuex";
+
+export default defineComponent({
+  computed: {
+    projectName(): string {
+      return this.projectInfo.name;
+    },
+    projectId(): string {
+      return this.projectInfo.id;
+    },
+    isReady(): boolean {
+      return this.projectInfo.items?.length !== 0;
+    },
+    ...mapState(["projectInfo"])
+  }
+});
+</script>
+
+<style></style>
